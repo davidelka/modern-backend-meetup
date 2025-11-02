8fa2aa7f2e15d224d33ce8151e45d27910978f75
David Elkayam <dodlike@gmail.com>
2025-11-02 13:31:21 +0200

    initial version

---
+++ src/main/java/com/modern/modernBE/ModernBeApplication.java
@@ -0,0 +1,13 @@
+package com.modern.modernBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modern/modernBE/ModernBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modern.modernBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
